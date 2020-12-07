7d00da4344089006a23d4f5042b1ab6b88e113bf
DESKTOP-GJBDRJT\s9420 <pcj0198@daum.net>
2020-12-08 07:05:32 +0900

    junk data delete

--- project_chanjong/settings.py
+++ project_chanjong/settings.py
@@ -131,23 +131,23 @@ WSGI_APPLICATION = 'project_chanjong.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'channjongp',
+        'USER': 'root',
+        'PASSWORD': 's9423093',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
-
-# "DATABASES" : {
-#     "default" : {
-#         "ENGINE" : "django.db.backends.mysql",
-#         "NAME" : "chanjongp",
-#         "USER" : "Chanjongp$chanjongp",
-#         "PASSWORD" : "s9423093",
-#         "HOST" : "localhost",
-#         "PORT" : "3306"
-#         }
-#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
